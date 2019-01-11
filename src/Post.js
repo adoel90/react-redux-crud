1fb36c7bb3425399cc5e546dc31792047ce1c08c
adoel90 <adoel.cs@gmail.com>
2019-01-11 15:07:47 +0700

    15.07

--- src/Post.js
+++ src/Post.js
@@ -7,13 +7,16 @@ return (
   <h2 className="post_title">{this.props.post.title}</h2>
   <p className="post_message">{this.props.post.message}</p>
   <div className="control-buttons">
-    <button className="edit"
-    onClick={() => this.props.dispatch({ type: 'EDIT_POST', id: this.props.post.id })
-    }
-    >Edit</button>
-    <button className="delete"
-    onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: this.props.post.id })}
-    >Delete</button>
+    <button 
+      className="edit" 
+      onClick={() => this.props.dispatch({ type: 'EDIT_POST', id: this.props.post.id })} > 
+        Edit
+    </button>
+    <button 
+      className="delete"
+      onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: this.props.post.id })} > 
+        Delete
+    </button>
   </div>
 </div>
 );
--- src/PostForm.js
+++ src/PostForm.js
@@ -22,12 +22,12 @@ render() {
 return (
 <div className="post-container">
   <h1 className="post_heading">Create Post</h1>
+  
   <form className="form" onSubmit={this.handleSubmit} >
-   <input required type="text" ref={(input) => this.getTitle = input}
-   placeholder="Enter Post Title" /><br /><br />
-   <textarea required rows="5" ref={(input) => this.getMessage = input}
-   cols="28" placeholder="Enter Post" /><br /><br />
-   <button>Post</button>
+    <input required type="text" ref={(input) => this.getTitle = input} placeholder="Enter Post Title" /><br /><br />
+    <textarea required rows="5" ref={(input) => this.getMessage = input} cols="28" placeholder="Enter Post" /><br /><br />
+    
+    <button>Post</button>
   </form>
 </div>
 );
